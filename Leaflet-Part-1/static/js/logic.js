b347404a3ef5fd1902532925e5065c4c6c3781c0
Michael <meleonard70@gmail.com>
2024-04-10 10:47:59 -0500

    final code update, .png files

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -4,64 +4,37 @@ let queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 // Use D3 to pull JSon data and store in variable data 
 d3.json(queryUrl).then
   (function (data) {
-    // Call createFeatures function and pass it data.features
+
+    // Call createFeatures function and pass it data.features pulled from JSon
     createFeatures(data.features);
 
     // Console log the data retrieved 
     console.log(data);
+
   });
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// Function to determine marker color by depth   
-function Get_Color_Depth(depth){              //Color Hex codes from https://www.w3schools.com/colors/colors_picker.asp    
-  if (depth > 90) return '#c20814';
-  else if (depth >= 70) return '#e35b07';
-  else if (depth >= 50) return '#ed8309';
-  else if (depth >= 30) return '#edc709';
-  else if (depth >= 10) return '#ede509';
-  else return '#4ded09';
+// Function to determine marker color by earthquake depth in kilometers
+function getColorDepth(depth){                // Color Hex codes from https://www.w3schools.com/colors/colors_picker.asp    
+  if (depth > 90) return '#c20814';           // red
+  else if (depth >= 70) return '#e35b07';     // red/orange
+  else if (depth >= 50) return '#ed8309';     // orange
+  else if (depth >= 30) return '#edc709';     // orange/yellow
+  else if (depth >= 10) return '#ede509';     // yellow
+  else return '#4ded09';                      // green
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-function createFeatures(earthquakeData) {
-
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array
-
-
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h2>Earthquake Recorded</h2><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p>
-    <p>Origination Depth: ${feature.geometry.coordinates[2]} kilometers</p><p>Epicenter Location: ${feature.properties.place}</p>`);
-  }
-  
-  
-  let earthquakeLayer = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature,
-      // Point to layer used to alter markers
-      pointToLayer: function(feature, latlng) {            //from https://gis.stackexchange.com/questions/360293/add-something-into-l-circlemarker
-        // Determine the style of markers based on properties
-      let marker = {
-          radius: feature.properties.mag * 25000,
-          fillColor: Get_Color_Depth(feature.geometry.coordinates[2]),   //From 3rd entry in Array
-          fillOpacity: 0.5,
-          color: '#000000',           //Color Hex code from https://www.w3schools.com/colors/colors_picker.asp
-          weight: 0.5
-        }
-      return L.circle(latlng,marker);
-    }
-  });
-
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakeLayer);
+// Function to multiply earthquake magnitude by 25000 to provide proportionality on the map
+function earthquakeMultiplyer(magnitude){
+  let multipliedMagnitude = magnitude * 25000
+  return multipliedMagnitude;
 }
 
 
@@ -70,41 +43,84 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakeLayer) {
 
-  // Create the base layers.
-  let mapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  // Create baseLayer of the map
+  let baseMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
 
-  // Create our map, giving it the streetmap and earthquakes layers
+  // Initiate creation of the map, giving it the baseLayer, earthquakeLayers, centering coordinates and zoom level
   let myMap = L.map('map', {
     center: [37.09, -95.71],   
     zoom: 5,
-    layers: [mapLayer, earthquakeLayer] 
-  
+    layers: [baseMapLayer, earthquakeLayer] 
   });
 
 
   // Add the legend
   let legend = L.control({position: 'bottomright'});
 
+  // Add onto legend setting div, the depth levels and initialize labels array
   legend.onAdd = function() {
     let div = L.DomUtil.create('div', 'info legend');
     let depth = [-10, 10, 30, 50, 70, 90];
     let labels = [];
   
+    //Set up Legend Title
     div.innerHTML = '<h2>Earthquake Depth</h2>';
 
+    // loop through depth array, get colors from , assemble HTML and append data to labels array 
     for (var i = 0; i < depth.length; i++) {
-      labels.push('<ul style="background-color:' + Get_Color_Depth(depth[i] +1) + '"> <span>' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '' : '+') + '</span></ul>');
+      labels.push('<ul style="background-color:' + getColorDepth(depth[i] +1) + '"> <span>' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '' : '+') + '</span></ul>');
     }
 
-    // add each label list item to the div under the <ul> tag
-      div.innerHTML += '<ul>' + labels.join('') + '</ul>';
-
+    // Connect labels information to <ul> tag to display on web page 
+    div.innerHTML += '<ul>' + labels.join('') + '</ul>';
     return div;
   };
+
+  //Adding the legend to the map
   legend.addTo(myMap);
 };
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Create earthquakeLayer to overlay on basic map
+
+function createFeatures(earthquakeData) {
+
+  // On each earthquake dataset use pointToLayer to create circle on earthquakeLayer with size based on the earthquake magnitude, and color based on earthquake depth in kilometers
+  let earthquakeLayer = L.geoJSON(earthquakeData, {onEachFeature: onEachFeature, pointToLayer: function(feature, latlng) {      //from https://gis.stackexchange.com/questions/360293/add-something-into-l-circlemarker 
+      // set markers based on earthquake variables                                                                              // & https://leafletjs.com/examples/geojson/
+      let marker = {                    
+        radius: earthquakeMultiplyer(feature.properties.mag),         // Call earthquakeMultiplyer to multiply earthquake magnitude by 25000 to provide proportionality on the map
+        fillColor: getColorDepth(feature.geometry.coordinates[2]),    // Pull from 3rd entry in array
+        fillOpacity: 0.5,                                             // Setting transparency of earthquake 
+        color: '#000000',                                             //setting black outer edge    //Color Hex code from https://www.w3schools.com/colors/colors_picker.asp
+        weight: 0.5                                                   // Setting weight of black edge
+      }
+      return L.circle(latlng,marker);
+  }});
+
+  
+   // Bind popup to each earthquake information to earthquake location
+                    // Header
+                    // Earthquake Date and Time
+                    // Earthquake Depth
+                    // Earthquake Location
+
+   function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h2>Earthquake Recorded</h2>                                             
+                     <p>Date: ${new Date(feature.properties.time)}</p>
+                     <p>Magnitude: ${feature.properties.mag}</p>
+                     <p>Origination Depth: ${feature.geometry.coordinates[2]} kilometers</p>
+                     <p>Epicenter Location: ${feature.properties.place}</p>`);
+  }
+
+  // Send our earthquakesLayer to the createMap function
+  createMap(earthquakeLayer);
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
